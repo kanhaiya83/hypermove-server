9e8d035f48cd0160e7627fe07b0a7a180a67ecd1
kanhaiya <kanhaiya2778@gmail.com>
2022-09-21 13:12:14 +0530

    cors bug

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ var corsOptions = {
 };
   
   app.use(cors(corsOptions));
+  
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const PORT=process.env.PORT || 5000;
