114e9cb4acdc8dd5f963ccdc581a1995045810b2
kanhaiya <kanhaiya2778@gmail.com>
2022-11-07 16:45:13 +0530

    inorbit game

--- config/database.js
+++ config/database.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
-require('dotenv').config(); 
+require("dotenv").config();
 
-let connUri=process.env.DB_URI
+let connUri = process.env.DB_URI;
 mongoose
   .connect(connUri, {
     useNewUrlParser: true,
-    useUnifiedTopology:true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to Mongoose");
@@ -20,55 +19,62 @@ mongoose
 // Creates simple schema for a User.  The hash and salt are derived from the user's given password when they register
 
 const scoreSchema = new Schema({
-  name: {type:String,required:true},
-  walletAddress: {type:String,required:true},
-  score: {type:String,required:true}  ,
-  isBanned:{type:Boolean,default:false},
-  createdAt:{type:Number}
-})
+  name: { type: String, required: true },
+  walletAddress: { type: String, required: true },
+  score: { type: String, required: true },
+  isBanned: { type: Boolean, default: false },
+  createdAt: { type: Number },
+});
 const partnerSchema = new Schema({
-  name:{type:String},
-  image:{type:String},
-  tags:{type:String},
-  twitter:{type:String},
-  website:{type:String}
-})
-const tournamentSchema= new Schema({
-  title:{type:String},
-  prize:{type:String},
-  entryFee:{
-    gems:{type:Number},
-    tickets:{type:Number}
+  name: { type: String },
+  image: { type: String },
+  tags: { type: String },
+  twitter: { type: String },
+  website: { type: String },
+});
+const tournamentSchema = new Schema({
+  title: { type: String },
+  prize: { type: String },
+  entryFee: {
+    gems: { type: Number },
+    tickets: { type: Number },
   },
-  playersCount:{type:Number,default:2},
-  title:{type:String},
-})
+  isFull:{type:Boolean,default:false},
+  createdBy:{type:String},
+  playersCount: { type: Number, default: 2 },
+  winner:{type:String},
+  joinedPlayers: [
+    {
+      userId: { type: String },
+      code: { type: String },
+      hasCompleted: { type: Boolean, default: false },
+      score: { type: Number },
+      timeJoined: { type: Number },
+    },
+  ],
+});
 const userSchema = new Schema({
-    name: {type:String},
-    userId: {type:String},
-    avatar: {type:String},
-    address: {type:String},
-    messageToSign: {type:String},
-    isMetamaskConnected: {type:Boolean},
-    isSteamConnected: {type:Boolean},
-    timecreated:{type:Number},
-    steamid:{type:String},
-    personaname:{type:String},
-    profileurl:{type:String},
-    referralCode:{type:String},
-    referredUsers:[{type:String}],
-    referredBy:{type:String,default:null},
-    score:{type:Number,default:0},
-    tickets:{type:Number},
-    gems:{type:Number},
-  })
-const UserModel=mongoose.model("SteamUser",userSchema)
-const ScoreModel = mongoose.model('Score', scoreSchema);
-const PartnersModel = mongoose.model('Partners', partnerSchema);
-const TournamentModel = mongoose.model('Tournament', tournamentSchema);
+  name: { type: String },
+  userId: { type: String },
+  avatar: { type: String },
+  address: { type: String },
+  messageToSign: { type: String },
+  isMetamaskConnected: { type: Boolean },
+  isSteamConnected: { type: Boolean },
+  timecreated: { type: Number },
+  steamid: { type: String },
+  personaname: { type: String },
+  profileurl: { type: String },
+  referralCode: { type: String },
+  referredUsers: [{ type: String }],
+  referredBy: { type: String, default: null },
+  score: { type: Number, default: 0 },
+  tickets: { type: Number },
+  gems: { type: Number },
+});
+const UserModel = mongoose.model("SteamUser", userSchema);
+const ScoreModel = mongoose.model("Score", scoreSchema);
+const PartnersModel = mongoose.model("Partners", partnerSchema);
+const TournamentModel = mongoose.model("Tournament", tournamentSchema);
 
-
-
-
-
-module.exports = {ScoreModel,UserModel,PartnersModel,TournamentModel};
+module.exports = { ScoreModel, UserModel, PartnersModel, TournamentModel };
--- routes/tournament.js
+++ routes/tournament.js
@@ -2,44 +2,171 @@ const express = require("express");
 const { TournamentModel, UserModel } = require("../config/database");
 const verifyJWT = require("../middlewares/verifyJWT");
 const router = express.Router();
+var randomstring = require("randomstring");
+router.get("/", async (req, res) => {
+  try {
+    const tournamentsList = await TournamentModel.find({});
+    return res.send({ success: true, tournamentsList });
+  } catch (e) {
+    console.log("Error in tournament/get =>", e);
+    res.status(500).send({ success: false });
+  }
+});
 
-router.get("/",async(req,res)=>{
+router.get("/joined",verifyJWT, async (req, res) => {
+  try {
+    const tournamentsList = await TournamentModel.find({
+      $or: [{ "joinedPlayers.userId": req.userId }],
+    });
 
-    try{
-const tournamentsList=await  TournamentModel.find({}) 
-return res.send({success:true,tournamentsList})
+    return res.send({ success: true, tournamentsList });
+  } catch (e) {
+    console.log("Error in tournament/get =>", e);
+    res.status(500).send({ success: false });
+  }
+});
+
+router.get("/verify/:code", async (req, res) => {
+  const { code } = req.params;
+  const tournament = await TournamentModel.findOne({
+    "joinedPlayers.code": code,
+  });
+  if (!tournament) {
+    return res.send({ success: false });
+  }
+  return res.send({
+    success: true,
+    tournament,
+  });
+});
+router.get("/submit", async (req, res) => {
+  const { code, score } = req.query;
+  const tournament = await TournamentModel.findOneAndUpdate(
+    {
+      "joinedPlayers.code": code,
+      "joinedPlayers.hasCompleted": false,
+    },
+    {$set:{ "joinedPlayers.$.hasCompleted": true, "joinedPlayers.$.score": score }},
+    { new: true }
+  );
+  if (!tournament) {
+    return res.send({ success: false });
+  }
+  if(tournament.joinedPlayers.length >1){
+    const player_1 =tournament.joinedPlayers[0]
+    const player_2 =tournament.joinedPlayers[1]
+    if(player_1.hasCompleted && player_2.hasCompleted){
+      if(player_1.score > player_2.score){
+        await TournamentModel.findByIdAndUpdate(tournament._id,{
+          winner: player_1.userId
+        })
+        await UserModel.findByIdAndUpdate(player_1.userId,{
+          $inc:{
+            gems:tournament.prize,
+            tickets:2*(tournament.entryFee.tickets),
+          }
+        })
+      }else if(player_2.score > player_1.score){
+        await TournamentModel.findByIdAndUpdate(tournament._id,{
+          winner: player_2.userId
+        })
+        
+        await UserModel.findByIdAndUpdate(player_2.userId,{
+          $inc:{
+            gems:tournament.prize,
+            tickets:2*(tournament.entryFee.tickets),
+          }
+        })
+      }else if(player_2.score === player_1.score){
+        await TournamentModel.findByIdAndUpdate(tournament._id,{
+          winner: player_2.userId
+        })
+        
+        await UserModel.findByIdAndUpdate(player_1.userId,{
+          $inc:{
+            gems:(tournament.prize)/2,
+            tickets:(tournament.entryFee.tickets),
+          }
+        })
+        
+        await UserModel.findByIdAndUpdate(player_2.userId,{
+          $inc:{
+            gems:(tournament.prize)/2,
+            tickets:(tournament.entryFee.tickets),
+          }
+        })
+      }
     }
-    catch(e){
-        console.log("Error in tournament/get =>",e);
-        res.status(500).send({success:false})
+  }
+  return res.send({
+    success: true,
+    tournament,
+  });
+});
+router.get("/join", verifyJWT, async (req, res) => {
+  const { tournamentId } = req.query;
+  const {userId} = req
+  const tournament = await TournamentModel.findOneAndUpdate(
+    {
+      _id: tournamentId,
+      isFull: false,
+    },
+    {
+      $push: {
+        joinedPlayers: {
+          userId,
+          code: randomstring.generate(10),
+          timeJoined: new Date().getTime(),
+        },
+      },
+      isFull:true
     }
-})
+  ,{new:true});
+  if (!tournament) {
+    return res.send({ success: false });
+  }
+  return res.send({
+    success: true,
+    tournament
+  });
+});
+router.post("/", verifyJWT, async (req, res) => {
+  // await UserModel.updateOne({_id:req.userId},{gems:1000,tickets:200    })
+  try {
+    const { entryFee } = req.body.tournamentData;
+    const gemsUsed = parseInt(entryFee.gems);
+    const ticketsUsed = parseInt(entryFee.tickets);
 
-
-
-router.post("/",verifyJWT,async(req,res)=>{
-// await UserModel.updateOne({_id:req.userId},{gems:100,tickets:20})
-    try{
-        const {entryFee}=req.body.tournamentData
-        const gemsUsed=  parseInt(entryFee.gems)
-        const ticketsUsed=  parseInt(entryFee.tickets)
-
-        const foundUser = await UserModel.findById(req.userId)
-        if(foundUser.gems< (gemsUsed) || foundUser.tickets < (ticketsUsed)){
-            return res.send({success:false})
-        }
-        const user = await UserModel.findByIdAndUpdate(req.userId,{$inc : {gems:(-gemsUsed),tickets:(-ticketsUsed)}},{new:true}) 
-        const newTour= new TournamentModel(req.body.tournamentData)
-        const savedTournament = await newTour.save()
-        return res.send({success:true,savedTournament,user})
+    const foundUser = await UserModel.findById(req.userId);
+    if (foundUser.gems < gemsUsed || foundUser.tickets < ticketsUsed) {
+      return res.send({ success: false });
     }
-    catch(e){
-        console.log("Error in tournament/get =>",e);
-        res.status(500).send({success:false})
-    }
-})
-router.get("/delete",async(req,res)=>{
- await    TournamentModel.deleteMany({})
- res.send("Done")
-})
-module.exports = router
+    const user = await UserModel.findByIdAndUpdate(
+      req.userId,
+      { $inc: { gems: -gemsUsed, tickets: -ticketsUsed } },
+      { new: true }
+    );
+    const data = {
+      joinedPlayers: [
+        {
+          userId: req.userId,
+          code: randomstring.generate(10),
+          timeJoined: new Date().getTime(),
+        },
+      ],
+      createdBy:req.userId,
+      ...req.body.tournamentData,
+    };
+    const newTour = new TournamentModel(data);
+    const savedTournament = await newTour.save();
+    return res.send({ success: true, savedTournament, user });
+  } catch (e) {
+    console.log("Error in tournament/get =>", e);
+    res.status(500).send({ success: false });
+  }
+});
+router.get("/delete", async (req, res) => {
+  await TournamentModel.deleteMany({});
+  res.send("Done");
+});
+module.exports = router;
